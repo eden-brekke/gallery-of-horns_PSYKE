512f7fc111e7b7f263e873640abd83c0f0e16eb8
Eden Brekke <eden.lorrai@gmail.com>
2022-03-15 15:00:53 -0700

    think... I'm done?

--- projects/courses/code-301/gallery-of-horns/src/Main.js
+++ projects/courses/code-301/gallery-of-horns/src/Main.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import HornedBeast from './HornedBeast.js';
-import Bernard from './goat-g810caf742_640.jpg';
-import Murray from './reindeer-g9e7fc1db9_640.jpg';
+import './Main.css';
 
 class Main extends React.Component {
   render() {
+    let beast = [];
+    this.props.data.forEach((animal, index) => {
+      beast.push(
+        <HornedBeast
+          title={animal.title}
+          name={animal.name}
+          image_url={animal.image_url}
+          description={animal.description}
+          key={index}
+        />
+      )
+    })
+
     return (
       <main>
-        <HornedBeast
-        title = "Murray The Reindeer"
-        imageURL = {Murray}
-        alt = "Murray"
-        description = "Murray Loves to Sing his heart out"/>
-        <HornedBeast
-         title = "Bernard the Goat"
-         imageURL = {Bernard}
-         alt="Bernard"
-         description = "Bernard is a model goat."/>
+        {beast}
       </main>
     );
   }
